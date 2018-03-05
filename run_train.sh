69ad39ecb3d8f091502e4a3d5519f84308b4132c
zhenheny <zhenheny@gmail.com>
2018-03-05 14:53:39 -0800

    continue train

--- run_train.sh
+++ run_train.sh
@@ -11,7 +11,7 @@ python3 train.py --dataset_dir=/home/zhenheng/datasets/kitti \
 	--img_grad_weight=0 \
 	--occ_mask=0.0 \
 	--depth_consistency=0.0 \
-	--gpu_id 0 --gpu_fraction 0.3\
+	--gpu_id 0 --gpu_fraction 0.4\
 	--rm_var_scope=/motion_net/,/edge/ \
 	--continue_train True
 	# --checkpoint_continue /home/zhenheng/tf_events/unsp_depth_normal/d2nn2d_4pt0.0_noflyout_dilated2_d2nnei3_n2dedgeremove_stereomono_ssim_wt0.1_depthsmooth_wedge3d_l2_alpha10_clip0_wt2_normal_wedgel2_alpha0.1_wt0.01_edge_input417_l2_deconvk4_noscaling_wt0.15_expwt0.0_sfmpy0723_depth1normal_eval/model-60001
